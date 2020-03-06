3ec0e3b19a135f0ff79c5d415b038199d24e8d02
Singh <765279@cognizant.com>
2020-03-06 19:39:01 +0530

    Submit

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -7,8 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 @RestController
 public class WelcomeController {
 
+
     private final String message;
-    
+
     public WelcomeController(@Value("${welcome.message}") String message) {
         this.message=message;
     }
