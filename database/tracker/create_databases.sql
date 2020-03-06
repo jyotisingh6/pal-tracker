dd4ef49bef3264e05193d32dacf8f43a89835b2e
Singh <765279@cognizant.com>
2020-03-06 13:26:30 +0530

    database file

--- database/tracker/create_databases.sql
+++ database/tracker/create_databases.sql
@@ -1,10 +1,10 @@
-DROP DATABASE IF EXISTS tracker_dev;
-DROP DATABASE IF EXISTS tracker_test;
+DROP DATABASE IF EXISTS tracker_dev;
+DROP DATABASE IF EXISTS tracker_test;
 
-CREATE DATABASE tracker_dev;
-CREATE DATABASE tracker_test;
+CREATE DATABASE tracker_dev;
+CREATE DATABASE tracker_test;
 
-CREATE USER IF NOT EXISTS 'root@localhost' IDENTIFIED BY 'root';
-
-GRANT ALL PRIVILEGES ON tracker_dev.* TO 'root@localhost';
-GRANT ALL PRIVILEGES ON tracker_test.* TO 'root@localhost';
+CREATE USER IF NOT EXISTS 'tracker'@'%'
+  IDENTIFIED BY '';
+GRANT ALL PRIVILEGES ON tracker_dev.* TO 'tracker' @'%';
+GRANT ALL PRIVILEGES ON tracker_test.* TO 'tracker' @'%';
